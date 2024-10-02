fc6199bd99fb5960276196f76a984ac30a936f67
patunganchelsea <patunganchelsea8@gmail.com>
2024-10-03 07:38:50 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,14 @@
+<div id = footer>
+    <footer>
+        Group 7
+    </footer>
+</div>
+
+<style>
+    footer{
+    text-align: center;
+    font-size: 3.5em;
+    color: #1f003b;
+    background-color: #FFEEBC;
+}
+</style>
