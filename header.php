19f7006847c291ca997e0200ed3c0a000dca307e
patunganchelsea <patunganchelsea8@gmail.com>
2024-10-03 07:38:13 +0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,15 @@
+<div id = header>
+    <header>
+        OUR TEAM
+    </header>
+</div>
+
+<style>
+    header {
+    width: 100%;
+    text-align: center;
+    font-size: 3.5em;
+    color: #1f003b;
+    background-color: #FFEEBC;
+    }
+</style>
